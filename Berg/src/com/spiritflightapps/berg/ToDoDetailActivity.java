edbf451f3db17f645e7bf9b6b7d98063c8146d62
Neil Warner <njwandroid@gmail.com>
2014-03-23 00:47:34 -0500

    Similar to vogella tutorial

---
+++ Berg/src/com/spiritflightapps/berg/ToDoDetailActivity.java
@@ -0,0 +1,121 @@
+package com.spiritflightapps.berg;
+
+import com.spiritflightapps.berg.contentprovider.MyToDoContentProvider;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/*
+ * TodoDetailActivity allows to enter a new todo item 
+ * or to change an existing
+ */
+public class ToDoDetailActivity extends Activity {
+  private EditText mTitleText;
+  private EditText mBodyText;
+
+  private Uri todoUri;
+
+  @Override
+  protected void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+    setContentView(R.layout.edit);
+
+    mTitleText = (EditText) findViewById(R.id.todo_edit_summary);
+    mBodyText = (EditText) findViewById(R.id.todo_edit_description);
+    Button confirmButton = (Button) findViewById(R.id.todo_edit_button);
+
+    Bundle extras = getIntent().getExtras();
+
+    // check from the saved Instance
+    todoUri = (bundle == null) ? null : (Uri) bundle
+        .getParcelable(MyToDoContentProvider.CONTENT_ITEM_TYPE);
+
+    // Or passed from the other activity
+    if (extras != null) {
+      todoUri = extras
+          .getParcelable(MyToDoContentProvider.CONTENT_ITEM_TYPE);
+
+      fillData(todoUri);
+    }
+
+    confirmButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        if (TextUtils.isEmpty(mTitleText.getText().toString())) {
+          makeToast();
+        } else {
+          setResult(RESULT_OK);
+          finish();
+        }
+      }
+
+    });
+  }
+
+  private void fillData(Uri uri) {
+    String[] projection = { TodoTable.COLUMN_SUMMARY,
+        TodoTable.COLUMN_DESCRIPTION };
+    Cursor cursor = getContentResolver().query(uri, projection, null, null,
+        null);
+    if (cursor != null) {
+      cursor.moveToFirst();
+     //tod: remvoe cat
+
+      mTitleText.setText(cursor.getString(cursor
+          .getColumnIndexOrThrow(TodoTable.COLUMN_SUMMARY)));
+      mBodyText.setText(cursor.getString(cursor
+          .getColumnIndexOrThrow(TodoTable.COLUMN_DESCRIPTION)));
+
+      // always close the cursor
+      cursor.close();
+    }
+  }
+
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    saveState();
+    outState.putParcelable(MyToDoContentProvider.CONTENT_ITEM_TYPE, todoUri);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    saveState();
+  }
+
+  private void saveState() {
+    String summary = mTitleText.getText().toString();
+    String description = mBodyText.getText().toString();
+
+    // only save if either summary or description
+    // is available
+
+    if (description.length() == 0 && summary.length() == 0) {
+      return;
+    }
+
+    ContentValues values = new ContentValues();
+    values.put(TodoTable.COLUMN_SUMMARY, summary);
+    values.put(TodoTable.COLUMN_DESCRIPTION, description);
+
+    if (todoUri == null) {
+      // New todo
+      todoUri = getContentResolver().insert(MyToDoContentProvider.CONTENT_URI, values);
+    } else {
+      // Update todo
+      getContentResolver().update(todoUri, values, null, null);
+    }
+  }
+
+  private void makeToast() {
+    Toast.makeText(ToDoDetailActivity.this, "Please maintain a summary",
+        Toast.LENGTH_LONG).show();
+  }
+} 
---
+++ Berg/src/com/spiritflightapps/berg/TodoTable.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.spiritflightapps.berg;
+
+/**
+ * these from http://www.vogella.com/tutorials/AndroidSQLite/article.html
+ */
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class TodoTable {
+
+  // Database table
+  public static final String TABLE_TODO = "todo";
+  public static final String COLUMN_ID = "_id";
+  public static final String COLUMN_SUMMARY = "summary";
+  public static final String COLUMN_DESCRIPTION = "description";
+
+  // Database creation SQL statement
+  private static final String DATABASE_CREATE = "create table " 
+      + TABLE_TODO
+      + "(" 
+      + COLUMN_ID + " integer primary key autoincrement, " 
+      + COLUMN_SUMMARY + " text not null," 
+      + COLUMN_DESCRIPTION
+      + " text not null" 
+      + ");";
+
+  public static void onCreate(SQLiteDatabase database) {
+	  Log.i("NJW","Creating database in table helper class with actual sql statement");
+    database.execSQL(DATABASE_CREATE);
+  }
+
+  public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+      int newVersion) {
+    Log.w(TodoTable.class.getName(), "Upgrading database from version "
+        + oldVersion + " to " + newVersion
+        + ", which will destroy all old data");
+    database.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+    onCreate(database);
+  }
+} 
