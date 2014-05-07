57d55ab3fb82617f544ba223ffb10d66ca7b5a2f
Neil Warner <njwandroid@gmail.com>
2014-05-07 14:20:35 -0500

    calculate code is back.

--- Berg/src/com/spiritflightapps/berg/TestAssessmentListActivity.java
+++ Berg/src/com/spiritflightapps/berg/TestAssessmentListActivity.java
@@ -10,13 +10,18 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Gallery;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.spiritflightapps.berg.contentprovider.AssessmentContentProvider;
 
@@ -44,6 +49,13 @@ public class TestAssessmentListActivity extends Activity implements
     private Gallery mListView;
     private ArrayList<EditText> mEditBoxes2;
 
+    ArrayList<ImageButton> instructionButtons;
+    ArrayList<String> mInstructions;
+//icon from http://www.flaticon.com/free-icon/falling-man-silhouette_11015
+
+    TextView tvTotal,tvTotal2;
+    //TODO: Goals column
+
     /**
      * Called when the activity is first created.
      */
@@ -54,7 +66,7 @@ public class TestAssessmentListActivity extends Activity implements
         setContentView(R.layout.test_listview);
         mListView = (Gallery) (findViewById(R.id.galleryTests));
         mListView.setSpacing(1);
-     //   mListView.setDividerHeight(2);
+        //   mListView.setDividerHeight(2);
 
         registerForContextMenu(mListView);
         String name = this.getIntent().getStringExtra(EXTRA_NAME);
@@ -77,7 +89,8 @@ public class TestAssessmentListActivity extends Activity implements
         mEditBoxes2.add((EditText) findViewById(R.id.editTextQ12_V2));
         mEditBoxes2.add((EditText) findViewById(R.id.editTextQ13_V2));
         mEditBoxes2.add((EditText) findViewById(R.id.editTextQ14_V2));
-
+        tvTotal2 = (TextView) findViewById(R.id.textViewTotalV2);
+        setupEditBoxes(mEditBoxes2, tvTotal2);
     }
 
 
@@ -109,14 +122,13 @@ public class TestAssessmentListActivity extends Activity implements
 
 
     private void createItem() {
-      //  createTestAssessment();
+        //  createTestAssessment();
         insertAssessment(getDateTime(), mEditBoxes2);
     }
 
     private void createTestAssessment() {
-        insertAssessment( getDateTime(), "1");
+        insertAssessment(getDateTime(), "1");
     }
-
 
 
     private String getDateTime() {
@@ -153,6 +165,7 @@ public class TestAssessmentListActivity extends Activity implements
         assessmentUri = getContentResolver().insert(AssessmentContentProvider.CONTENT_URI, values);
 
     }
+
     private void insertAssessment(String date, String a1) {
         ContentValues values = new ContentValues();
         values.put(AssessmentTable.COLUMN_DATE, date);
@@ -169,9 +182,9 @@ public class TestAssessmentListActivity extends Activity implements
 
         // Fields from the database (projection)
         // Must include the _id column for the adapter to work
-        String[] from = new String[] { AssessmentTable.COLUMN_DATE, AssessmentTable.COLUMN_Q1, AssessmentTable.COLUMN_Q2, AssessmentTable.COLUMN_Q3 };
+        String[] from = new String[]{AssessmentTable.COLUMN_DATE, AssessmentTable.COLUMN_Q1, AssessmentTable.COLUMN_Q2, AssessmentTable.COLUMN_Q3};
         // Fields on the UI to which we map
-        int[] to = new int[] { R.id.TextViewDate,R.id.TextViewQ1, R.id.TextViewQ2, R.id.TextViewQ3 };
+        int[] to = new int[]{R.id.TextViewDate, R.id.TextViewQ1, R.id.TextViewQ2, R.id.TextViewQ3};
 
         getLoaderManager().initLoader(0, null, this);
         adapter = new SimpleCursorAdapter(this, R.layout.list_row_test, null, from,
@@ -203,4 +216,87 @@ public class TestAssessmentListActivity extends Activity implements
         adapter.swapCursor(null);
     }
 
+    private void setupEditBoxes(ArrayList<EditText> editBoxesColumn, TextView tvTotal) {
+        //TODO: autofill goal column
+
+        final ArrayList<EditText> editBoxes = editBoxesColumn;
+        final TextView textViewTotal = tvTotal;
+
+        for (int i = 0; i < editBoxes.size(); i++) {
+            final int next = i + 1;
+            final EditText e = editBoxes.get(i);
+            e.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    int answer = Integer.MIN_VALUE;
+                    String error = "";
+                    if (s.length() == 0) {
+                        return; //backspace to correct
+                    }
+                    try {
+                        answer = Integer.parseInt(s.toString());
+                    } catch (Exception e) {
+                        error = "Please enter a number.";
+                    }
+                    //TODO: strings.xml
+                    if (answer > 4) {
+                        error = "Please enter 0-4.";
+                    }
+
+                    if (error.length() > 0) {
+                        Toast.makeText(TestAssessmentListActivity.this.getApplicationContext(), error, Toast.LENGTH_LONG).show();
+
+                    } else {
+                        //autoadvance
+                        if (next < editBoxes.size()) {
+                            EditText nextEditBox = editBoxes.get(next);
+                            nextEditBox.requestFocus();
+                        }
+                        //autocalculate
+
+                        calculateTotalIfAllFilledIn(editBoxes, textViewTotal);
+                    }
+
+
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count,
+                                              int after) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+        }
+
+
+    }
+
+    private void calculateTotalIfAllFilledIn(ArrayList<EditText> editBoxes, TextView textViewTotal) {
+        int total = 0;
+        int score = 0;
+        try {
+            for (EditText editBox : editBoxes) {
+                score = Integer.parseInt(editBox.getText().toString());
+                if (score > 4) {
+                    throw (new Exception()); //TODO: Detail which one via tag in xml
+                }
+                total += score;
+            }
+            //  Toast.makeText(AssessmentActivity.this.getApplicationContext(), "Total Score=" + total, Toast.LENGTH_SHORT).show();
+            //TODO: Just remove? or use only the first time?
+            textViewTotal.setText(String.valueOf(total));
+
+        } catch (Exception e) {
+            //Validation done in each field. if blank or one missing, do nothing
+
+        }
+    }
 }
